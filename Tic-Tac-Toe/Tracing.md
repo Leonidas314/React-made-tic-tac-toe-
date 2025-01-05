a6e744ce932b5d6044049185daae1ccb3df8d0b3
Leon <leondellafiore2016@gmail.com>
2025-01-05 08:52:40 -0300

    Conditional rendering for know which piece is next to move

--- Tic-Tac-Toe/Tracing.md
+++ Tic-Tac-Toe/Tracing.md
@@ -4,3 +4,22 @@ Using vite, do:
 
 npm create vite@latest ->Project name ->Package name (optional)
 ->Select framework (React here) -> Select lenguage (JavaScript+SWC)
+
+The project start creating a game board by 3x3, this is an 9 size array rendered by an Square component
+for each element of the array, this squares are on an section styled by grid css property. In this case 
+we are using the index of the array element just like the key identifier.
+
+The initial state of elements is null, for this useState(Array(9).fill(null))...
+
+Conditional Rendering:
+
+It is possible by using the trinary operator to stablish a determined className component.
+In the line 7 we have :
+const Square =({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ?'is-selected' : ''}`
+  ...
+  <div className = {className}>
+  ...
+  }
+This mean that the Square component evaluates the `isSelected` property, if this one is true the className will be `square is-selected`, if not just `square`, and this will be affected by css styled
+in diferent ways.
--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from "react"
+
 const TURNS = {
   X:'X',
   O:'O'
 }
 
-const board = Array(9).fill(null);
 
-const Square = ({ children ,updateBoard, index})=> {
+const Square = ({ children ,isSelected,updateBoard, index})=> {
+  const className=`square ${isSelected ? 'is-selected' : ''}`
   return (
-    <div className="square">
+    <div className={className}>
       {children}
     </div>
   )
-
 }
+
 function App(){
+  const [board,setBoard] = useState(Array(9).fill(null));
+  const [turn,setTurn] = useState(TURNS.X)
   return (
     <main className = 'board'>
       <h1>Tic - Tac - Toe</h1>
@@ -23,16 +27,23 @@ function App(){
           return (
            <Square 
            key={index} 
-           index={index}
+           index={index}  
            >
-            {index}
+            {board[index]}
           </Square>
           )
         })
       }
       </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        
+      </section>
     </main>
   )
+
 }
 
 export default App
